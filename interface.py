46d21cc784c4eb80cf11bb048a003d77fe9fe67c
Tulio Cunha <44248592+tulioac@users.noreply.github.com>
2020-10-22 08:59:08 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -8,7 +8,7 @@ password_i = input()
 print('Horario de matricula (Siga o formato "DD/MM/AAAA HH:MM:SS):')
 horario_i = input()
 
-print('Agora digite a cada linha as cadeiras que você gostaria de se matricular e em qual turma, garanta que tenha vaga ainda, não tenha conflito entre elas e o numero minimo de creditos. Siga o formato "CCCCCCC - TT", quando terminar digite "sair"')
+print('Agora digite a cada linha as cadeiras que você gostaria de se matricular e em qual turma, garanta que tenha vaga ainda, não tenha conflito entre elas e o numero mínimo de creditos. Siga o formato "CCCCCCC - TT", quando terminar digite "sair"')
 
 chairs = []
 
@@ -20,4 +20,4 @@ while True:
 
 realizarMatricula(matricula_i,password_i,horario_i,chairs)
 
-print('Matricula realizada, queira verificar por favor.\nUm oferecimento de PATATIRO e PATATRAFICO')
+print('Matrícula realizada, queira verificar por favor.\nUm oferecimento de PATATIRO e PATATRAFICO')
